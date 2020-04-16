1b1baa79e106d36b0108b4194c28dc82376d9b43
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-04-16 16:28:50 -0400

    DO NOT PULL THIS FILE TO MASTER

--- proj4.cpp
+++ proj4.cpp
@@ -14,15 +14,19 @@ using namespace std;
 //argc is the count of arguments
 //argv are the parameters themselves
 int main(int argc, char* argv[]) {
-  srand(time(NULL)); //Seeds the random number generator
-  cout << "Welcome to UMBC Encryption" << endl;
-  while (argc < 2) { //Checks to make sure the user passes a file to load
-    cout << "You are missing a data file." << endl;
-    cout << "Expected usage ./proj4 proj4_test1.txt" << endl;
-    cout << "The input file should have some strings to encrypt or decrypt" << endl;
+    srand(time(NULL)); //Seeds the random number generator
+    cout << "Welcome to UMBC Encryption" << endl;
+    // commented out for VStudio only!!
+    /*
+    while (argc < 2) { //Checks to make sure the user passes a file to load
+        cout << "You are missing a data file." << endl;
+        cout << "Expected usage ./proj4 proj4_test1.txt" << endl;
+        cout << "The input file should have some strings to encrypt or decrypt" << endl;
+        return 0;
+    }*/
+
+    // Implement which files you want here 
+    CipherTool myCipher("proj4_test4.txt"); //Creates a new ciphertool passing the filename
+    myCipher.Start(); //Starts the cipherTool
     return 0;
-  }
-  CipherTool myCipher(argv[1]); //Creates a new ciphertool passing the filename
-  myCipher.Start(); //Starts the cipherTool
-  return 0;
 }
