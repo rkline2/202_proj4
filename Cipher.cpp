31542cc87e27a7eafec8c76a0b00650595e592c4
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-04-19 16:23:42 -0400

    version 1.2

--- Cipher.cpp
+++ Cipher.cpp
@@ -24,61 +24,50 @@ Cipher::Cipher(string message, bool isEncrypted) {
     // Desc - Virtual destructor
     // Preconditions - A derived class is being deleted
     // Postconditions - A base class is deleted
-Cipher::~Cipher() {}
-
-// Name: Decrypt
-    // Desc - A purely virtal function to decrypt a message
-    // Preconditions - The message is not already decrypted
-    // Postconditions - A message is decrypted based on the child class
-void Cipher::Decrypt() {}
-
-// Name: Encrypt
-    // Desc - A purely virtal function to encrypt a message
-    // Preconditions - The message is not already encrypted
-    // Postconditions - A message is encrypted based on the child class
-void Cipher::Encrypt() {}
+Cipher::~Cipher() {
+    m_isEncrypted = false;
+    m_message.erase();
+}
 
 // Name: GetMessage
     // Desc - Returns the message
     // Preconditions - The message exists
     // Postconditions - A message is returned
-//string Cipher::GetMessage() {}
+string Cipher::GetMessage() { return m_message; }
 
 // Name: GetIsEncrypted
     // Desc - Returns isEncrypted (0 is not encrypted and 1 is encrypted)
     // Preconditions - The cipher exists
     // Postconditions - A bool is returned
-//bool Cipher::GetIsEncrypted() {}
+bool Cipher::GetIsEncrypted() { return m_isEncrypted; }
 
 // Name: SetMessage
     // Desc - Updates a message
     // Preconditions - The message exists
     // Postconditions - A message is updated
-void Cipher::SetMessage(string message) {}
+void Cipher::SetMessage(string message) { m_message = message; }
 
 // Name: ToggleEncrypted
     // Desc - Turns (true to false) or (false to true)
     // Preconditions - The cipher exists
     // Postconditions - The m_isEncrypted is toggled
-void Cipher::ToggleEncrypted() {}
-
-// Name: ToString
-    // Desc - A purely virtual function that returns the string of the object type
-    // Preconditions - The object exists
-    // Postconditions - The subtype is returned
-//string Cipher::ToString() {}
-
-// Name: FormatOutput
-    // Desc - A purely virtual function that returns the formatted data for Output
-    // Preconditions - The object exists
-    // Postconditions - The type, message, and key are output
-//string Cipher::FormatOutput() {}
-
+void Cipher::ToggleEncrypted() {
+    if (m_isEncrypted) { m_isEncrypted = false; }
+    else { m_isEncrypted = true; }
+}
 // Name: Overloaded << Operator
     // Desc - Outputs the message
     // Preconditions - The object exists
     // Postconditions - The message from the object is returned
-//ostream& operator<<(ostream& output, Cipher& C) {}
+ostream& operator<<(ostream& output, Cipher& C) {
+    if (C.GetMessage().at(0) == QUOTE) {
+        return output << C.GetMessage() << " (" << C.ToString() << ")";
+    }
+    else { 
+        return output << "\"" << C.GetMessage() << "\"" << " (" << C.ToString() << ")"; 
+    }
+    
+}
 
 /*string m_message; //The message being encrypted/decrypted
     bool m_isEncrypted; //Indicates if the message is encrypted/decrypted*/
