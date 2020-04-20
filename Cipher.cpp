c48b1a8d8e637f2d892a9eb8298874da637ab687
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-04-19 21:50:00 -0400

    Final update

--- Cipher.cpp
+++ Cipher.cpp
@@ -1,73 +1,70 @@
+/*****************************************
+** File:    Cipher.cpp
+** Project: CMSC 202 Project 4, Spring 2020
+** Author:  Rooklyn Kline
+** Date:    4/21/20
+** Section: 02
+** E-mail:  rkline2@umbc.edu
+**
+** This file contains the base class "Cipher".
+** This class contains the message and if the cipher 
+** has been encrypted or not for every cipher. 
+** The primary functions from this class are the getters 
+** and setters for the m variables. 
+**
+***********************************************/
 #include "Cipher.h"
 #include <iostream>
 using namespace std;
 
-// Name: Cipher (Default Constructor)
-    // Desc - Stores a message that is either read to be encrypted or already encrypted
-    // Preconditions - None
-    // Postconditions - A message is stored in a Cipher object
+// Cipher (Default Constructor)
+    // A message is stored in a Cipher object
 Cipher::Cipher() {
     m_message = "test";
     m_isEncrypted = 0;
 }
 
-// Name: Cipher (Overloaded Constructor)
-    // Desc - Stores a message that is either read to be encrypted or already encrypted
-    // Preconditions - A message is passed to this constructor, and if it is encrypted
-    // Postconditions - A message is stored in a Cipher object
+// Cipher (Overloaded Constructor)
+    // Given the message and if it's encrypted, 
+    // the message and isEncrypted is stored in a Cipher object
 Cipher::Cipher(string message, bool isEncrypted) {
     m_message = message;
     m_isEncrypted = isEncrypted;
 }
 
-// Name: ~Cipher (Destructor)
-    // Desc - Virtual destructor
-    // Preconditions - A derived class is being deleted
-    // Postconditions - A base class is deleted
+// ~Cipher (Destructor)
+    // A base class is deleted
 Cipher::~Cipher() {
     m_isEncrypted = false;
     m_message.erase();
 }
 
-// Name: GetMessage
-    // Desc - Returns the message
-    // Preconditions - The message exists
-    // Postconditions - A message is returned
+// GetMessage
+    // A message is returned
 string Cipher::GetMessage() { return m_message; }
 
-// Name: GetIsEncrypted
-    // Desc - Returns isEncrypted (0 is not encrypted and 1 is encrypted)
-    // Preconditions - The cipher exists
-    // Postconditions - A bool is returned
+// GetIsEncrypted
+    // returns a bool if the message is encrypted or not
 bool Cipher::GetIsEncrypted() { return m_isEncrypted; }
 
-// Name: SetMessage
-    // Desc - Updates a message
-    // Preconditions - The message exists
-    // Postconditions - A message is updated
+// SetMessage
+    // Given a new message, the message is updated
 void Cipher::SetMessage(string message) { m_message = message; }
 
-// Name: ToggleEncrypted
-    // Desc - Turns (true to false) or (false to true)
-    // Preconditions - The cipher exists
-    // Postconditions - The m_isEncrypted is toggled
+// ToggleEncrypted
+    // Turns m_isEncrypted (true to false) or (false to true)
 void Cipher::ToggleEncrypted() {
     if (m_isEncrypted) { m_isEncrypted = false; }
     else { m_isEncrypted = true; }
 }
-// Name: Overloaded << Operator
-    // Desc - Outputs the message
-    // Preconditions - The object exists
-    // Postconditions - The message from the object is returned
+
+// Overloaded << Operator
+    // Given the object, the formatted message from the object is returned
 ostream& operator<<(ostream& output, Cipher& C) {
     if (C.GetMessage().at(0) == QUOTE) {
         return output << C.GetMessage() << " (" << C.ToString() << ")";
     }
     else { 
         return output << "\"" << C.GetMessage() << "\"" << " (" << C.ToString() << ")"; 
-    }
-    
+    }   
 }
-
-/*string m_message; //The message being encrypted/decrypted
-    bool m_isEncrypted; //Indicates if the message is encrypted/decrypted*/
